1743fccf04947b420ddc3a5a3b761e07e098d401
Isaac Johnston <isaac@superstruct.nz>
2020-03-16 16:03:45 +1300

    Attempt to reproduce using event handlers
    At the time Gregg and myself had demonstrated the issue in CLJS using
    event handler for worksheets onAdded but subsequently narrowed it down
    to simpler cases.
    
    Does not reproduce in JavaScript.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,6 +6,7 @@
 /* global console, document, Excel, Office */
 
 Office.onReady(info => {
+  console.info("v1");
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
@@ -13,22 +14,36 @@ Office.onReady(info => {
   }
 });
 
+async function onWorksheetAdded(event) {
+  let context = event.context;
+  let worksheetId = event.worksheetId;
+  console.info("Added worksheet ID:", worksheetId);
+  try {
+    await Excel.run(context, async context => {
+      let addedWorksheet = context.workbook.worksheets.getItemOrNullObject(worksheetId);
+      console.log("here", worksheetId);
+      addedWorksheet.load("name");
+      await context.sync();
+      if (!addedWorksheet.isNullObject) {
+        console.info("Added worksheet name:", addedWorksheet.name);
+        addedWorksheet.visibility = Excel.SheetVisibility.hidden;
+        addedWorksheet.delete();
+        await context.sync();
+      } else {
+        console.warn("Added worksheet is null object.");
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function run() {
   try {
     await Excel.run(async context => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
+      context.workbook.worksheets.onAdded.add(onWorksheetAdded);
       await context.sync();
-      console.log(`The range address was ${range.address}.`);
+      console.info("worksheets.onAdded event handler added.")
     });
   } catch (error) {
     console.error(error);
